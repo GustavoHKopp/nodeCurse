67487e97e842527d8934fecd53f45a50f5b75b13
GustavoHKopp <gustavohkopp@gmail.com>
2023-02-15 20:21:03 -0300

    Starting get data and sending to database

--- questions-and-answers_bootstrap/Index.js
+++ questions-and-answers_bootstrap/Index.js
@@ -1,16 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
 
+//Informando para o Express usar o EJS como View Engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+//Rotas
 app.get('/', (req, res) => {
   res.render("index")
 })
 
 app.get('/perguntar',(req,res) =>{
   res.render("perguntar")
+})
+
+app.post('/salvarpergunta', (req, res) => {
+  let titulo = req.body.titulo
+  let descricao = req.body.descricao
+  res.send(`Formulario recebido! titulo: ${titulo}, descricao: ${descricao}`)
 })
 
 app.listen(port,function(err){
