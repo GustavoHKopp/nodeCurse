37ae9095b64ae73bad12afbc9a3efc28d195ee22
GustavoHKopp <85900782+GustavoHKopp@users.noreply.github.com>
2023-03-30 11:03:13 -0300

    Starting to use Sequelize for connect in the Mysql

--- questions-and-answers_bootstrap/Index.js
+++ questions-and-answers_bootstrap/Index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
+const connection = require('./database/database')
+const perguntaModel = require('./database/Pergunta')
+
+//database
+connection
+    .authenticate()
+    .then(() => console.log('conexao com o mysql ocorreu conforme o esperado'))
+    .catch((err) => console.log(`algo de errado nao esta certo. <br> erro: ${err}`))
 
 //Informando para o Express usar o EJS como View Engine
 app.set('view engine', 'ejs')
@@ -21,7 +29,7 @@ app.get('/perguntar',(req,res) =>{
 app.post('/salvarpergunta', (req, res) => {
   let titulo = req.body.titulo
   let descricao = req.body.descricao
-  res.send(`Formulario recebido! titulo: ${titulo}, descricao: ${descricao}`)
+  res.send(`Formulario recebido! <hr> titulo: ${titulo}<br> descricao: ${descricao}`)
 })
 
 app.listen(port,function(err){
